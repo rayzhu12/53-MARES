dc0e504a7eb42f6f3241880be34725112649ac63
Michelle Liu <michelleliu343@gmail.com>
2018-02-12 20:54:10 -0500

    login/register screen
    import project and launch login/register screen, when "sign in" button is clicked, login activity launched

---
+++ HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.michelleliu.homelessapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void signIn(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
