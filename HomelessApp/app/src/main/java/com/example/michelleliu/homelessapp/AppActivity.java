3ce670bf6fbb246891f30ab2ba243559c4e5bb3d
ew2886 <31673898+ew2886@users.noreply.github.com>
2018-02-13 21:48:46 -0500

    created logout button

--- HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/AppActivity.java
+++ HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/AppActivity.java
@@ -1,11 +1,13 @@
 package com.example.michelleliu.homelessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class AppActivity extends AppCompatActivity {
 
@@ -22,6 +24,14 @@ public class AppActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AppActivity.this, MainActivity.class));
             }
         });
     }
