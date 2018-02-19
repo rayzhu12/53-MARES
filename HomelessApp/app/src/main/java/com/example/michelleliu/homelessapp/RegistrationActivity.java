56e7d9092de76cf996135ef1b8619e3eb3d0fd03
ew2886 <31673898+ew2886@users.noreply.github.com>
2018-02-19 18:17:45 -0500

    Registration Submit Button first try

--- HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/RegistrationActivity.java
+++ HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/RegistrationActivity.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import model.Model;
+import model.UserInfo;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    @Override
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private Button register;
+
+    private UserInfo user;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
@@ -26,6 +42,7 @@ public class RegistrationActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
 //        Button cancel = (Button) findViewById(R.id.cancel_button);
 //        cancel.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -36,6 +53,27 @@ public class RegistrationActivity extends AppCompatActivity {
 //                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
 //            }
 //        });
+
+        name = (EditText) findViewById(R.id.Name);
+        email =(EditText) findViewById(R.id.Email);
+        password = (EditText) findViewById(R.id.Password);
+
+        register = (Button) findViewById(R.id.create_account);
+        register.setOnClickListener(addNewUser);
     }
+
+    private View.OnClickListener addNewUser = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Model model = Model.getInstance();
+            user = new UserInfo("", "", "");
+
+            user.setName(name.getText().toString());
+            user.setEmail(email.getText().toString());
+            user.setPassword(password.getText().toString());
+
+            model.checkUser(user);
+        }
+    };
 
 }
---
+++ HomelessApp/app/src/main/java/model/UserInfo.java
@@ -0,0 +1,40 @@
+package model;
+
+/**
+ * Created by Emily Wang on 2/19/2018.
+ */
+
+public class UserInfo {
+    private String name;
+    private String email;
+    private String password;
+
+    public UserInfo(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String geEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString() {
+        return ("Name: " + name + "Username: " + email + "Password: " + password);
+    }
+ }
