8a5634721a0db82b27dd837630ac90e6c886c96a
tuanyuan2008 <schen1337@gmail.com>
2018-02-19 14:35:08 -0500

    designed registration page (malfunctioning buttons)

--- HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/RegistrationActivity.java
+++ HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/RegistrationActivity.java
@@ -1,11 +1,13 @@
 package com.example.michelleliu.homelessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -24,6 +26,16 @@ public class RegistrationActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+//        Button cancel = (Button) findViewById(R.id.cancel_button);
+//        cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                mEmailView.setText("");
+////                mPasswordView.setText("");
+////                mPasswordView.clearFocus();
+//                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+//            }
+//        });
     }
 
 }
