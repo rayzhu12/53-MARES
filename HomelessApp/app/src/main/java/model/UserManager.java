cc9463e5ca14d1dd376b4d2ffb7315c87be8f699
ew2886 <31673898+ew2886@users.noreply.github.com>
2018-03-26 21:47:30 -0400

    added shelters to database

--- HomelessApp/app/src/main/java/model/UserManager.java
+++ HomelessApp/app/src/main/java/model/UserManager.java
@@ -2,7 +2,6 @@ package model;
 
 import android.util.Log;
 
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,10 +32,14 @@ public class UserManager {
 
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
+                if (dataSnapshot.child("test1").exists()) {
+                    Log.d(TAG, "true");
+                }
                 Log.d(TAG, "onDataChange: Added information to database: \n" +
                         dataSnapshot.getValue());
             }
