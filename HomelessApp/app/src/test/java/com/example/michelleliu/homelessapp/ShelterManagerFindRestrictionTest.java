3ce79ec2ea832b7b59eb2fcdfe52f1c7794f9796
tuanyuan2008 <schen1337@gmail.com>
2018-04-08 20:36:27 -0400

    fixed logic for search

---
+++ HomelessApp/app/src/test/java/com/example/michelleliu/homelessapp/ShelterManagerFindRestrictionTest.java
@@ -0,0 +1,21 @@
+package com.example.michelleliu.homelessapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Zhuo.C on 4/8/2018.
+ */
+
+public class ShelterManagerFindRestrictionTest {
+    String[] female = {"Covenant House Georgia", "Young Adult Guidance Center", "The Atlanta Day " +
+            "Center for Women & Children", "My Sister's House", "Eden Village"};
+    String[] male = {"Young Adult Guidance Center", "Covenant House Georgia", "The Shepherd's Inn",
+            "Gateway Center", "Homes of Light"};
+    String[] nonbinary = {};
+    String[] families = {"Atlanta's Children Center", "Our House", "Nicholas House"};
+    String[] ya = {"Covenant House Georgia", "Young Adult Guidance Center"};
+    String[] children = {"Our House"};
+
+
+}
