9c4171a2a6e4ef738e2a0715098c85536295459e
tuanyuan2008 <schen1337@gmail.com>
2018-04-08 20:45:45 -0400

    hardcode

---
+++ HomelessApp/app/src/test/java/com/example/michelleliu/homelessapp/ShelterManagerFindRestrictionTest.java
@@ -0,0 +1,18 @@
+package com.example.michelleliu.homelessapp;
+
+/**
+ * Created by Zhuo.C on 4/8/2018.
+ */
+
+public class ShelterManagerFindRestrictionTest {
+    String[] female = {"My Sister's House", "The Atlanta Day Center for Women & Children",
+            "Eden Village", "Hope Atlanta"};
+    String[] male = {"The Shepherd's Inn", "Fuqua Hall", "Hope Atlanta", "Gateway Center"};
+    String[] nonbinary = {"Hope Atlanta"};
+    String[] families = {"Atlanta's Children Center", "Our House", "Nicholas House",
+            "Hope Atlanta"};
+    String[] ya = {"Covenant House Georgia", "Hope Atlanta", "Young Adult Guidance Center"};
+    String[] children = {"My Sister's House", "The Atlanta Day Center for Women & Children",
+            "Atlanta's Children Center", "Eden Village", "Our House",
+            "Covenant House Georgia", "Hope Atlanta"};
+}
