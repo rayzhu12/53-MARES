9739123080c4aa95d8280d8cb4fd71642e2c6f6f
ew2886 <31673898+ew2886@users.noreply.github.com>
2018-04-25 16:13:36 -0400

    push notifications are working!
    added services to Manifest for FCM
    added stuff to gradle for FCM
    added two activities, Messaging Service and ID which deal with receiving notifs

---
+++ HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/MyFirebaseInstanceIDService.java
@@ -0,0 +1,28 @@
+package com.example.michelleliu.homelessapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+
+        //For registration of token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //To displaying token on logcat
+        Log.d("TOKEN: ", refreshedToken);
+
+    }
+
+}
