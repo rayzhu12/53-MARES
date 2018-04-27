e723cc740cab810d11f76f4e1201537b884e9477
ew2886 <31673898+ew2886@users.noreply.github.com>
2018-04-27 01:25:11 -0400

    facebook Auth working

--- HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/MainActivity.java
+++ HomelessApp/app/src/main/java/com/example/michelleliu/homelessapp/MainActivity.java
@@ -2,22 +2,43 @@ package com.example.michelleliu.homelessapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONObject;
 
 /**
  * Main activity
  * @author snack overflow
  */
 public class MainActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
 
     // Creating EditText .
     private EditText email;
@@ -50,10 +71,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
 
-//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-//        userDatabase = FirebaseDatabase.getInstance();
-//        userDatabaseReference = userDatabase.getReference("users");
+        LoginButton fbLogIn = (LoginButton) findViewById(R.id.fb_login_button);
+        fbLogIn.setReadPermissions("email", "public_profile");
+        fbLogIn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("Facebook Auth", "Hello" + loginResult.getAccessToken().getToken());
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("FacebookAuth", "onCancel section");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("Facebook Auth", "in error section", error);
+            }
+        });
 
         // Assigning layout email ID and Password ID.
         email = findViewById(R.id.email);
@@ -130,6 +168,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("Facebook Auth", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.w("Facebook Auth", "signInWithCredential", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                } else {
+                    String uid = task.getResult().getUser().getUid();
+                    String name = task.getResult().getUser().getDisplayName();
+                    String email = task.getResult().getUser().getEmail();
+                    Log.d("Facebook Auth", "Name: " + name + ", Email: " + email);
+
+                    // Finishing current Main Activity.
+                    finish();
+
+                    // Opening the Login Activity using Intent.
+                    Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 
     private void CheckEditTextIsEmptyOrNot(){
         // Getting value form Email's EditText and fill into EmailHolder string variable.
